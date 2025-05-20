9eb59243c59cba4792e8a072ceb8002c8671d092
R-FUETA <raul.a.fuenzalida@gmail.com>
2025-05-20 15:46:30 -0400

    1.0.0

---
+++ src/main/java/com/edutechms2/Edutechms2/Edutechms2Application.java
@@ -0,0 +1,13 @@
+package com.edutechms2.Edutechms2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Edutechms2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Edutechms2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/edutechms2/Edutechms2/Edutechms2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutechms2.Edutechms2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Edutechms2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
